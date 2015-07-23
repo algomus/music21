008bc09468ae0cf3a04d30c2d1a2976a6b3c157e
Mathieu Giraud <mathieu@algomus.fr>
2015-07-23 15:22:50 +0200

    Revert "tinyNotation.py: tinyNotationWriter, transform a Stream into a TinyNotation"
    Pre-merge of v2.0.5
    This reverts commit cd4c3b47cb572a67c8fdfa3686b6baaf3cd04c32.

--- music21/tinyNotation.py
+++ music21/tinyNotation.py
@@ -630,38 +630,6 @@ class TinyNotationException(exceptions21.Music21Exception):
     pass
 
 
-class TinyNotationWriter(object):
-    def _durationToTinyNotation(self, dur):
-        (qLen, durType, dots, tupleNumerator, tupletDenominator, tupletType) = duration.unitSpec(dur)
-        numberType = duration.convertTypeToNumber(durType)
-
-        return '%d' % numberType + '.' * dots
-
-
-    def _noteToTinyNotation(self, noteObj):
-        if noteObj.octave <= 3:
-            return noteObj.name[0] * (4 - noteObj.octave) + noteObj.name[1:]
-        else:
-            name = noteObj.name.lower()
-            return name[0] + "'" * (noteObj.octave - 4) + name[1:]
-
-    def streamToTinyNotation(self, streamObj):
-        l = []
-        for elem in streamObj.flat:
-            if isinstance(elem, note.Note):
-                s = self._noteToTinyNotation(elem)
-                d = self._durationToTinyNotation(elem.duration)
-                l.append(s + d)
-            elif isinstance(elem, note.Rest):
-                s = 'r'
-                d = self._durationToTinyNotation(elem.duration)
-                l.append(s + d)
-            elif isinstance(elem, meter.TimeSignature):
-                s = elem.ratioString
-                l.append(s)
-
-        return ' '.join(l)
-
 
 #-------------------------------------------------------------------------------
 class Test(unittest.TestCase):
@@ -747,12 +715,6 @@ Total duration of Stream: 6.0
         self.assertEqual(nst1[2].isRest, True)
         self.assertEqual(nst1[5].name, "G")
         self.assertEqual(nst1[7].name, "A-")
-
-    def testTinyNotationWriter(self):
-        text = "3/4 d8 e4 F2 g'8 AA8 b''16"
-        s = TinyNotationStream(text)
-        text2 = TinyNotationWriter().streamToTinyNotation(s)
-        self.assertEqual(text, text2)
 
 class TestExternal(unittest.TestCase):    
 
